5691f5189fef7f10ec437e2f94f438b3dd5deae9
bleistivt <bleistivt@users.noreply.github.com>
2015-09-12 10:08:07 +0200

    array sytax

--- class.datejumper.plugin.php
+++ class.datejumper.plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-$PluginInfo['DateJumper'] = array(
+$PluginInfo['DateJumper'] = [
     'Version' => '1.7',
     'Name' => 'Date Jumper',
     'Description' => 'Places a date label above discussions and comments to separate them by age.',
@@ -9,19 +9,22 @@ $PluginInfo['DateJumper'] = array(
     'MobileFriendly' => true,
     'Author' => 'peregrine',
     'License' => 'GNU GPL2'
-);
+];
 
 class DateJumperPlugin extends Gdn_Plugin {
 
     private $keepDate;
 
+
     public function assetModel_styleCss_handler($sender) {
         $sender->addCssFile('datejumper.css', 'plugins/DateJumper');
     }
 
+
     private function addResources($sender) {
         $sender->addJsFile('datejumper.js', 'plugins/DateJumper');
     }
+
 
     /* DiscussionsController */
 
@@ -51,6 +54,7 @@ class DateJumperPlugin extends Gdn_Plugin {
         $this->discussionDateHeading($sender, $args, true);
     }
 
+
     /* DiscussionController */
 
     public function discussionController_render_before($sender) {
@@ -65,13 +69,14 @@ class DateJumperPlugin extends Gdn_Plugin {
         if ($date != $this->keepDate) {
             $this->keepDate = $date;
             if (!strpos($date, ':')) {
-                echo wrap(wrap($date, 'div', array('class' => 'CommentDateSpacer')), 'li');
+                echo wrap(wrap($date, 'div', ['class' => 'CommentDateSpacer']), 'li');
             } elseif (!$this->today) {
-                echo wrap(wrap(t('Today'), 'div', array('class' => 'CommentDateSpacer')), 'li');
+                echo wrap(wrap(t('Today'), 'div', ['class' => 'CommentDateSpacer']), 'li');
                 $this->today = true;
             }
         }
     }
+
 
     private function discussionDateHeading($sender, $args, $inCategory = false) {
         if (!C('Plugins.DateJumper.ShowInDiscussions')) {
@@ -83,29 +88,30 @@ class DateJumperPlugin extends Gdn_Plugin {
         $date = Gdn_Format::date($args['Discussion']->LastDate);
         if ($date != $this->keepDate) {
             if (!strpos($date, ':')) {
-                echo wrap(wrap($date, 'span', array('class' => 'DiscussionDateSpacer')), 'li');
+                echo wrap(wrap($date, 'span', ['class' => 'DiscussionDateSpacer']), 'li');
             } elseif (!strpos($this->keepDate, ':')) {
-                echo wrap(wrap(t('Today'), 'span', array('class' => 'DiscussionDateSpacer')), 'li');
+                echo wrap(wrap(t('Today'), 'span', ['class' => 'DiscussionDateSpacer']), 'li');
             }
             $this->keepDate = $date;
         }
     }
+
 
     public function settingsController_dateJumper_create($sender) {
         $sender->permission('Garden.Settings.Manage');
         $sender->addSideMenu();
 
         $conf = new ConfigurationModule($sender);
-        $conf->initialize(array(
-            'Plugins.DateJumper.ShowInDiscussions' => array(
+        $conf->initialize([
+            'Plugins.DateJumper.ShowInDiscussions' => [
                 'Control' => 'CheckBox',
                 'LabelCode' => 'Show Date Jumper Labels on Discussion Topic Pages'
-            ),
-            'Plugins.DateJumper.ShowInComments' => array(
+            ],
+            'Plugins.DateJumper.ShowInComments' => [
                 'Control' => 'CheckBox',
                 'LabelCode' => 'Show Date Jumper Labels within Comments in a Discussion'
-            )
-        ));
+            ]
+        ]);
 
         $sender->title('Date Jumper');
         $sender->setData('Description', 'See readme for instructions for further details.');
